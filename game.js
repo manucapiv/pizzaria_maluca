a6d430e88f10a3914189cbdaa2a8a0e915e731fb
Emerson <emersonat@ig.com.br>
2019-04-27 16:06:01 -0300

    add layout escolha jogador

--- game.js
+++ game.js
@@ -9,41 +9,41 @@ const game = {
     ],
 
     casas: [
-        {numero: 1, nome:  "Milho", coordenada: { top: 5, left: 49 }, temJogador: false },
-        {numero: 2, nome: "Escolha um ingrediente", coordenada: { top: 6, left: 56 }, temJogador: false },
-        {numero: 3, nome: "Sorte ou azar", coordenada: { top: 7, left: 64 }, temJogador: false },
-        {numero: 4, nome: "Brocolis", coordenada: { top: 11, left: 70 }, temJogador: false },
-        {numero: 5, nome: "Sorte ou azar", coordenada: { top: 16, left: 76 }, temJogador: false },
-        {numero: 6, nome: "Ovo", coordenada: { top: 21, left: 81 }, temJogador: false },
-        {numero: 7, nome: "Cebola", coordenada: { top: 28, left: 85 }, temJogador: false },
-        {numero: 8, nome: "Sorte ou azar", coordenada: { top: 35, left: 88 }, temJogador: false },
-        {numero: 9, nome: "Peperoni", coordenada: { top: 42, left: 90 }, temJogador: false },
-        {numero: 10, nome: "Sorte ou azar", coordenada: { top: 50, left: 90 }, temJogador: false },
-        {numero: 11, nome: "Presunto", coordenada: { top: 58, left: 90 }, temJogador: false },
-        {numero: 12, nome: "Escolha um ingrediente", coordenada: { top: 66, left: 88 }, temJogador: false },
-        {numero: 13, nome: "Queijo", coordenada: { top: 73, left: 84 }, temJogador: false },
-        {numero: 14, nome: "Ervilha", coordenada: { top: 79, left: 79 }, temJogador: false },
-        {numero: 15, nome: "Sorte ou azar", coordenada: { top: 84, left: 74 }, temJogador: false },
-        {numero: 16, nome: "Brocolis", coordenada: { top: 88, left: 68 }, temJogador: false },
-        {numero: 17, nome: "Azeitona", coordenada: { top: 91, left: 61 }, temJogador: false },
-        {numero: 18, nome: "Sorte ou azar", coordenada: { top: 93, left: 54 }, temJogador: false },
-        {numero: 19, nome: "Perde tudo", coordenada: { top: 93, left: 46 }, temJogador: false },
-        {numero: 20, nome: "Cebola", coordenada: { top: 91, left: 39 }, temJogador: false },
-        {numero: 21, nome: "Sorte ou azar", coordenada: { top: 89, left: 32 }, temJogador: false },
-        {numero: 22, nome: "Peperoni", coordenada: { top: 85, left: 26 }, temJogador: false },
-        {numero: 23, nome: "Queijo", coordenada: { top: 79, left: 20 }, temJogador: false },
-        {numero: 24, nome: "Sorte ou azar", coordenada: { top: 73, left: 15 }, temJogador: false },
-        {numero: 25, nome: "Sorte ou azar", coordenada: { top: 66, left: 12 }, temJogador: false },
-        {numero: 26, nome: "Tomate", coordenada: { top: 59, left: 10 }, temJogador: false },
-        {numero: 27, nome: "Ovo", coordenada: { top: 51, left: 9 }, temJogador: false },
-        {numero: 28, nome: "Azeitona", coordenada: { top: 43, left: 9 }, temJogador: false },
-        {numero: 29, nome: "Sorte ou azar", coordenada: { top: 36, left: 11 }, temJogador: false },
-        {numero: 30, nome: "Sorte ou azar", coordenada: { top: 11, left: 28 }, temJogador: false },
-        {numero: 31, nome: "Milho", coordenada: { top: 22, left: 18 }, temJogador: false },
-        {numero: 32, nome: "Ervilha", coordenada: { top: 16, left: 23 }, temJogador: false },
-        {numero: 33, nome: "Sorte ou azar", coordenada: { top: 11, left: 28 }, temJogador: false },
-        {numero: 34, nome: "Presunto", coordenada: { top: 8, left: 35 }, temJogador: false },
-        {numero: 35, nome: "Tomate", coordenada: { top: 6, left: 42 }, temJogador: false }
+        {numero: 1, nome:  "Milho", coordenada: { top: 5, left: 49 }, jogadores:  [] },
+        {numero: 2, nome: "Escolha um ingrediente", coordenada: { top: 6, left: 56 }, jogadores:  [] },
+        {numero: 3, nome: "Sorte ou azar", coordenada: { top: 7, left: 64 }, jogadores:  [] },
+        {numero: 4, nome: "Brocolis", coordenada: { top: 11, left: 70 }, jogadores:  [] },
+        {numero: 5, nome: "Sorte ou azar", coordenada: { top: 16, left: 76 }, jogadores:  [] },
+        {numero: 6, nome: "Ovo", coordenada: { top: 21, left: 81 }, jogadores:  [] },
+        {numero: 7, nome: "Cebola", coordenada: { top: 28, left: 85 }, jogadores:  [] },
+        {numero: 8, nome: "Sorte ou azar", coordenada: { top: 35, left: 88 }, jogadores:  [] },
+        {numero: 9, nome: "Peperoni", coordenada: { top: 42, left: 90 }, jogadores:  [] },
+        {numero: 10, nome: "Sorte ou azar", coordenada: { top: 50, left: 90 }, jogadores:  [] },
+        {numero: 11, nome: "Presunto", coordenada: { top: 58, left: 90 }, jogadores:  [] },
+        {numero: 12, nome: "Escolha um ingrediente", coordenada: { top: 66, left: 88 }, jogadores:  [] },
+        {numero: 13, nome: "Queijo", coordenada: { top: 73, left: 84 }, jogadores:  [] },
+        {numero: 14, nome: "Ervilha", coordenada: { top: 79, left: 79 }, jogadores:  [] },
+        {numero: 15, nome: "Sorte ou azar", coordenada: { top: 84, left: 74 }, jogadores:  [] },
+        {numero: 16, nome: "Brocolis", coordenada: { top: 88, left: 68 }, jogadores:  [] },
+        {numero: 17, nome: "Azeitona", coordenada: { top: 91, left: 61 }, jogadores:  [] },
+        {numero: 18, nome: "Sorte ou azar", coordenada: { top: 93, left: 54 }, jogadores:  [] },
+        {numero: 19, nome: "Perde tudo", coordenada: { top: 93, left: 46 }, jogadores:  [] },
+        {numero: 20, nome: "Cebola", coordenada: { top: 91, left: 39 }, jogadores:  [] },
+        {numero: 21, nome: "Sorte ou azar", coordenada: { top: 89, left: 32 }, jogadores:  [] },
+        {numero: 22, nome: "Peperoni", coordenada: { top: 85, left: 26 }, jogadores:  [] },
+        {numero: 23, nome: "Queijo", coordenada: { top: 79, left: 20 }, jogadores:  [] },
+        {numero: 24, nome: "Sorte ou azar", coordenada: { top: 73, left: 15 }, jogadores:  [] },
+        {numero: 25, nome: "Sorte ou azar", coordenada: { top: 66, left: 12 }, jogadores:  [] },
+        {numero: 26, nome: "Tomate", coordenada: { top: 59, left: 10 }, jogadores:  [] },
+        {numero: 27, nome: "Ovo", coordenada: { top: 51, left: 9 }, jogadores:  [] },
+        {numero: 28, nome: "Azeitona", coordenada: { top: 43, left: 9 }, jogadores:  [] },
+        {numero: 29, nome: "Sorte ou azar", coordenada: { top: 36, left: 11 }, jogadores:  [] },
+        {numero: 30, nome: "Sorte ou azar", coordenada: { top: 28, left: 14 }, jogadores:  [] },
+        {numero: 31, nome: "Milho", coordenada: { top: 22, left: 18 }, jogadores:  [] },
+        {numero: 32, nome: "Ervilha", coordenada: { top: 16, left: 23 }, jogadores:  [] },
+        {numero: 33, nome: "Sorte ou azar", coordenada: { top: 11, left: 28 }, jogadores:  [] },
+        {numero: 34, nome: "Presunto", coordenada: { top: 8, left: 35 }, jogadores:  [] },
+        {numero: 35, nome: "Tomate", coordenada: { top: 6, left: 42 }, jogadores:  [] }
     ],
 
    
@@ -84,14 +84,20 @@ const game = {
             numeroNovaCasa = numeroNovaCasa - this.casas.length;
         }
 
-        casa = this.casas[numeroNovaCasa - 1];
-        this.jogadores[jogador.numero - 1].casa = casa;
-        
-        
+        novaCasa = this.casas[numeroNovaCasa - 1];
+        jogador.casa.jogadores.
+        jogador.casa = novaCasa;
+         
         div_player = this.containner.querySelector("#tabuleiro").querySelector("#player" + jogador.numero);
         div_player.style.left = jogador.casa.coordenada.left + "%";
         div_player.style.top =  jogador.casa.coordenada.top + "%";
 
+    },
+
+    array_teste: [1,2,3],
+
+    teste: function(array){
+        array.push(4);
     },
 
     desenharJogadores: function () {
@@ -99,15 +105,6 @@ const game = {
             jogador = this.jogadores[i];
             div_player = this.containner.querySelector("#tabuleiro").querySelector("#player" + jogador.numero);
             div_player.style.backgroundColor = jogador.cor;
-        }
-    },
-
-    criarCasa: function (numero, nome, coordenada) {
-        return {
-            numero: numero,
-            nome: nome,
-            temJogador: false,
-            coordenada: coordenada
         }
     },
 
